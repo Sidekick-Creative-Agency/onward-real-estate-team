d7e8b56375fdb94eeb79eb83bf2c213e2e66b58c
Aron Hawkins <hawkins.aron@gmail.com>
2026-02-04 09:23:14 -0600

    skip beforeOperationHook during static generation

--- src/collections/Attachments/hooks/beforeOperationHook.ts
+++ src/collections/Attachments/hooks/beforeOperationHook.ts
@@ -2,6 +2,14 @@ import { getMeUser } from '@/utilities/getMeUser'
 import { CollectionBeforeOperationHook } from 'payload'
 
 export const beforeOperationHook: CollectionBeforeOperationHook = async ({ args, req }) => {
+  const isStaticGeneration =
+    process.env.NEXT_PHASE === 'phase-production-build' ||
+    process.env.NEXT_PHASE === 'phase-export' ||
+    process.env.NEXT_EXPORT === 'true'
+  if (isStaticGeneration) {
+    return
+  }
+
   try {
     const user = await getMeUser()
     req.user = {
